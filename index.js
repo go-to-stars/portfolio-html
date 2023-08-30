2debc3c0262dd7eacb78b8e13dd161bdde066871
go-to-stars <ivanstep@ukr.net>
2023-08-31 02:24:03 +0300

    attached description and photo

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const mainContent = document.querySelector(".main");
+// const mainContent = document.querySelector(".main");
+const mainContent = document.querySelector("body");
 const sideBar = document.querySelector(".sidebar");
 
 let winWidth = window.innerWidth;
-if (winWidth > 767.98) {
-  const mainContentHeight = mainContent.offsetHeight;
-  sideBar.style.height = `${mainContentHeight}px`;
+let mainContentHeight = 0;
+
+function contentHeight() {
+  winWidth = window.innerWidth;
+  if (winWidth > 767.98) {
+    const mainContentHeight = mainContent.offsetHeight;
+    sideBar.style.height = `${mainContentHeight}px`;
+  } else {
+    sideBar.style.height = ``;
+  }
 }
+
+const timerId = setTimeout(contentHeight, 50); // завдання висоти sideBar на старті сторінки
 
 window.addEventListener("resize", () => {
   winWidth = window.innerWidth;
@@ -16,13 +26,14 @@ window.addEventListener("resize", () => {
   } else {
     sideBar.style.height = ``;
   }
-});
+}); // завдання висоти sideBar при зміні розподільної здатності сторінки
 
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
+    modalBackdropLink: document.querySelector(".modal-backdrop"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
@@ -35,8 +46,13 @@ window.addEventListener("resize", () => {
     refs.modal.classList.toggle("is-hidden");
 
     const scrollLockMethod = !isModalOpen
-      ? "disableBodyScroll"
-      : "enableBodyScroll";
-    bodyScrollLock[scrollLockMethod](document.body);
+      ? (document.body.style.overflow = "hidden") // заблокувати скролл
+      : (document.body.style.overflow = "scroll"); // розблокувати скролл
   }
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      toggleModal();
+    }
+  });
 })();
